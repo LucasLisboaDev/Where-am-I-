fc13c58886cf04eb902c194906992254b5d2e364
Lucas Lisboa Alves <82411386+LucasLisboaDev@users.noreply.github.com>
2024-02-17 11:47:57 -0500

    inntial; commit
    check that out

--- Where am I?/ViewController.swift
+++ Where am I?/ViewController.swift
@@ -6,11 +6,40 @@
 //
 
 import UIKit
+import CoreLocation
+import MapKit
 
 class ViewController: UIViewController {
+    
+  
+    @IBOutlet weak var latitude: UILabel!
+    @IBOutlet weak var longitude: UILabel!
+    
+    @IBOutlet weak var textAddress: UITextField!
+    @IBAction func findOut(_ sender: UIButton) { let geocoder = CLGeocoder()
+        if let address = textAddress.text {
+            geocoder.geocodeAddressString(address) { [weak self] (placemarks, error) in
+                if let placemark = placemarks?.first, let location = placemark.location {
+                    self?.updateUIWithCoordinates(location.coordinate)
+                } else {
+                    // Handle error or no location found
+                }
+            }
+        }
+    }
+    
+    let locationManager = CLLocationManager()
+    
+    func updateUIWithCoordinates(_ coordinate: CLLocationCoordinate2D) {
+        latitude.text = "Latitude: \(coordinate.latitude)"
+        longitude.text = "Longitude: \(coordinate.longitude)"
+    }
 
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        print("This is a test for git nad github")
+        locationManager.requestWhenInUseAuthorization()
         // Do any additional setup after loading the view.
     }
 
